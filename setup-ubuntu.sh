8e52ec74159b01199b0d093965fb0ffce6e8fddc
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2017-10-30 12:29:09 +0200

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 apt-get install python3-pip -y
 pip3 install homeassistant
-wget -O  https://raw.githubusercontent.com/Tech-Blog/Home-assistant/master/Hass-Startup-Script /etc/init.d/hass
-cd /etc/init.d folder
+cd /etc/init.d
+wget -O  https://raw.githubusercontent.com/Tech-Blog/Home-assistant/master/Hass-Startup-Script hass
 sudo chmod a+x hass
 sudo ./hass start
 sudo ./hass stop
