a43dd3493b70c9804cf70499624c7676c9e5b321
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2017-10-30 12:09:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 apt-get install python3-pip
 pip3 install homeassistant
+wget -O  https://raw.githubusercontent.com/Tech-Blog/Home-assistant/master/Hass-Startup-Script /etc/init.d/hass
+cd /etc/init.d folder
+sudo chmod a+x hass
+sudo ./hass start
+sudo ./hass stop
+sudo update-rc.d hass defaults
